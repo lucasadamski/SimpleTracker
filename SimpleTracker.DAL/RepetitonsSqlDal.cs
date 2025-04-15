c6c28d1e4f934c5f70f69492178cbac14d291e1e
Lucas Adams <lucasadams@protonmail.com>
2025-04-15 22:08:40 +0100

    new REpetitions Dal

---
+++ SimpleTracker.DAL/RepetitonsSqlDal.cs
@@ -0,0 +1,13 @@
+﻿using SimpleTracker.DAL.Interfaces;
+using SimpleTracker.DTO;
+
+namespace SimpleTracker.DAL
+{
+    public class RepetitonsSqlDal : IRepetitonsSqlDal
+    {
+        public CreateNewRepetitionsResult CreateNewRepetitions(Repetitions repetitions)
+        {
+            return new CreateNewRepetitionsResult();
+        }
+    }
+}
