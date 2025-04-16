0c44e834554113f7aec2b91adaa684e5cb12811b
Lucas Adams <lucasadams@protonmail.com>
2025-04-16 22:15:44 +0100

    post deployment script

---
+++ SimpleTracker.DB/Script.PostDeployment1.sql
@@ -0,0 +1,28 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select 1 from [dbo].[Unit])
+begin
+    insert into [dbo].[Unit] (Name)
+    values ('Reps'), 
+        ('Minutes'),
+        ('Hours')
+end
+
+
+if not exists (select 1 from [dbo].[Activity])
+begin
+    insert into [dbo].[Activity] (Name, UnitId)
+    values ('Push-ups', 1), 
+        ('Running', 2),
+        ('Reading', 2)
+end
