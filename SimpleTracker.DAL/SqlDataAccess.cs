45de44c32c85443326fc66b5f3c367e51a79b076
Lucas Adams <lucasadams@protonmail.com>
2025-04-17 17:58:16 +0100

    SQL generic abstraction middleware

---
+++ SimpleTracker.DAL/SqlDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using Dapper;
+using System.Data;
+using SimpleTracker.DAL.Interfaces;
+
+namespace SimpleTracker.DAL
+{
+    public class SQLDataAccess : ISQLDataAccess
+    {
+        private string _connectionString;
+
+        public SQLDataAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure,
+                                                         U parameters,
+                                                         string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_connectionString);
+
+            return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string storedProcedure,
+                                     T parameters,
+                                     string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_connectionString);
+
+            await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+
+}
