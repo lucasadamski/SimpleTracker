5d3ab41c2bf93dbc6f58c6a5e1a9fbf95b84ee35
Lucas Adams <lucasadams@protonmail.com>
2025-04-18 11:37:31 +0100

    minor change

--- SimpleTracker.DAL/ActivitySqlDal.cs
+++ SimpleTracker.DAL/ActivitySqlDal.cs
@@ -5,9 +5,16 @@ namespace SimpleTracker.DAL
 {
     public class ActivitySqlDal : IActivitySqlDal
     {
-        public NewActivityResult CreateNewActivity(Activity activity)
+        private readonly ISQLDataAccess _db;
+        public ActivitySqlDal(ISQLDataAccess db)
         {
-            return new NewActivityResult();
+            _db = db;
         }
+
+        public NewActivityResult CreateNewActivity(Activity activity) =>
+            (NewActivityResult)_db.SaveData(storedProcedure: "dbo.spActivity_Insert", new { activity.Name, activity.UnitId });
+
+        public IEnumerable<Activity> GetAllActivities() =>
+            _db.LoadData<Activity, dynamic>(storedProcedure: "dbo.Activity_GetAll", new { });
     }
 }
