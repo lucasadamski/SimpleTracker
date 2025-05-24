78de82181d579c242ab4a2d887850b204d7addd1
Lucas Adams <lucasadams@protonmail.com>
2025-05-24 13:22:28 +0100

    Renamed stored procs in DAL

--- SimpleTracker.DAL/ActivitySqlDal.cs
+++ SimpleTracker.DAL/ActivitySqlDal.cs
@@ -12,14 +12,14 @@ namespace SimpleTracker.DAL
 
         public bool CreateNewActivity(Activity activity)
         {
-            var result = _db.SaveData(storedProcedure: "[SimpleTrackerDev].[dbo].[s]pActivity_Insert]", new { activity.Name, activity.UnitId });
+            var result = _db.SaveData(storedProcedure: "[dbo].[spActivity_Insert]", new { activity.Name, activity.UnitId });
             _logger.LogDebug("dbo.spActivity_Insert {Name} {UnitId} returned {Result}", activity.Name, activity.UnitId, result);
             return result;
         }
 
         public IEnumerable<Activity> GetAllActivities()
         {
-            var result = _db.LoadData<Activity, dynamic>(storedProcedure: "[SimpleTrackerDev].[dbo].[spActivity_GetAll]", new { });
+            var result = _db.LoadData<Activity, dynamic>(storedProcedure: "[dbo].[spActivity_GetAll]", new { });
             _logger.LogDebug("dbo.Activity_GetAll returned {ResultCount} items", result.Count());
             return result;
         }
--- SimpleTracker.DAL/EntrySqlDal.cs
+++ SimpleTracker.DAL/EntrySqlDal.cs
@@ -11,14 +11,14 @@ namespace SimpleTracker.DAL
 
         public bool CreateNewEntry(Entry entry)
         {
-            var result = _db.SaveData(storedProcedure: "[SimpleTrackerDev].[dbo].[spEntry_Insert]", new { entry.Value, entry.ActivityId });
+            var result = _db.SaveData(storedProcedure: "[dbo].[spEntry_Insert]", new { entry.Value, entry.ActivityId });
             _logger.LogDebug("dbo.spEntry_Insert {Value} returned {Result}", entry.Value, result);
             return result;
         }
               
         public IEnumerable<Entry> GetAllEntries()
         {
-            var result = _db.LoadData<Entry, dynamic>(storedProcedure: "[SimpleTrackerDev].[dbo].[spEntry_GetAll]", new { });
+            var result = _db.LoadData<Entry, dynamic>(storedProcedure: "[dbo].[spEntry_GetAll]", new { });
             _logger.LogDebug("dbo.Entry_GetAll returned {ResultCount} items", result.Count());
             return result;
         }
@@ -29,7 +29,7 @@ namespace SimpleTracker.DAL
 
             try
             {
-                var spResult = _db.LoadData<EntrySummaryAllTime, dynamic>(storedProcedure: "[SimpleTrackerDev].[dbo].[spSummary_Get]", new { });
+                var spResult = _db.LoadData<EntrySummaryAllTime, dynamic>(storedProcedure: "[dbo].[spSummary_Get]", new { });
                 result = spResult.Select(n => n.Name + " " + n.Value.ToString() + " " + n.Reps).ToList();
 
                 _logger.LogDebug("[dbo].[spEntry_GetSummaryAllTime] returned {ResultCount} items", result.Count());
