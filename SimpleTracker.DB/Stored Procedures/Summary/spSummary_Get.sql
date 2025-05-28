92178e41e6401532c47dbb51a6ef7bbca7d4e73d
Lucas Adams <lucasadams@protonmail.com>
2025-05-28 22:17:44 +0100

    Refactored stored proc

--- SimpleTracker.DB/Stored Procedures/Summary/spSummary_Get.sql
+++ SimpleTracker.DB/Stored Procedures/Summary/spSummary_Get.sql
@@ -1,8 +1,22 @@
-﻿CREATE PROCEDURE [dbo].[spSummary_Get]
+﻿create procedure [dbo].[spSummary_Get]
+@from datetime = null,
+@to datetime = null
 AS	
 	
-SELECT a.Name as [Activity], sum(e.Value) as [Value], 'Reps' as [Unit] -- to fix
-FROM Entry e
-join Activity a on a.Id = e.ActivityId
-join Unit u on u.Id = a.UnitId
-group by a.Name
+if (@from is null and @to is null) 
+begin
+	select a.Name as [Activity], sum(isnull(e.Value, 0)) as [Value], u.Name as [Unit]
+	from Entry e
+	right outer join Activity a on a.Id = e.ActivityId
+	join Unit u on u.Id = a.UnitId
+	group by a.Name, u.Name
+end
+else 
+begin
+	select a.Name as [Activity], sum(isnull(e.Value, 0)) as [Value], u.Name as [Unit]
+	from Entry e
+	right outer join Activity a on a.Id = e.ActivityId
+	join Unit u on u.Id = a.UnitId
+	where e.DateAdded <= @from AND e.DateAdded >= @to
+	group by a.Name, u.Name
+end
