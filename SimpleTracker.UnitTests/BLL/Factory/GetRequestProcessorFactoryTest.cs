da93cba9c93a81ddc0155d6d6b0d6b58d8e92d35
Lucas Adams <lucasadams@protonmail.com>
2025-05-30 22:02:25 +0100

    unit test

---
+++ SimpleTracker.UnitTests/BLL/Factory/GetRequestProcessorFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using SimpleTracker.BLL.DTO;
+using SimpleTracker.BLL.Factory;
+using SimpleTracker.DTO;
+using FluentAssertions;
+using SimpleTracker.BLL.RequestProcessor.Get;
+
+namespace SimpleTracker.UnitTests.BLL.Factory
+{
+    public class GetRequestProcessorFactoryTest
+    {
+        private Mock<ILogger> loggerMock = new Mock<ILogger>();
+
+        [Fact]
+        public void ReturnGetRequestProcessor_GetsValidInput_ReturnsCorrectFactory()
+        {
+            // Arrange
+            var response = new Response()
+            {
+                RequestVerb = RequestVerb.Get,
+                Type = typeof(Entry)
+            };
+            var getRequestProcessor = new GetRequestProcessorFactory(loggerMock.Object);
+            var expectedResult = typeof(Entry);
+
+            // Act
+            var actualResult = getRequestProcessor.ReturnGetRequestProcessor(response);
+
+            // Assert
+            actualResult.Should().BeOfType<EntryGetRequestProcessor>();
+        }
+    }
+}
