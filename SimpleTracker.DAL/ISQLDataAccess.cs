8648772a8ddb8d954951522ed73133a64c682ecc
Lucas Adams <lucasadams@protonmail.com>
2025-08-27 10:22:47 +0100

    SQL Data Access

---
+++ SimpleTracker.DAL/ISQLDataAccess.cs
@@ -0,0 +1,7 @@
+namespace SimpleTracker.DAL;
+
+public interface ISQLDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
+}
