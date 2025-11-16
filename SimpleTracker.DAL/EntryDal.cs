c1dd241c45ac347ce9241838ff960ed6b61489b3
Lucas <lucasadams@protonmail.com>
2025-11-16 12:04:08 +0000

    Implemented DAL for Entry

--- SimpleTracker.DAL/EntryDal.cs
+++ SimpleTracker.DAL/EntryDal.cs
@@ -1,13 +1,93 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 using SimpleTracker.DAL.Interfaces;
+using SimpleTracker.DTO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace SimpleTracker.DAL
 {
-    public class EntryDal : DalBase
+    public class EntryDal : DalBase, IEntryDal
     {
         public EntryDal(ISqlDataAccess db, ILogger logger) : base(db, logger)
         {
-            // todo write EntryDal body, so that test will pass
+
         }
+        // implement EntryDal 
+        // 
+
+        public bool CreateNewEntry(Entry entry)
+        {
+            var result = false;
+            try
+            {
+                result = _db.SaveData(storedProcedure: "[dbo].[spEntry_Insert]", new { entry.Value, entry.ActivityId, entry.DateAdded }); // sql signature return: bit
+                _logger.LogDebug("[dbo].[spEntry_Insert] Value: {Value} ActivityId: {ActivityId} returned {Result}", entry.Value, entry.ActivityId, result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            
+            return result;
+        }
+        public Entry ReadEntry(int id, string userId)
+        {
+            var result = new Entry();
+            try
+            {
+                result = _db.LoadData<Entry, dynamic>(storedProcedure: "[dbo].[spEntry_Read]", new { id, userId }).FirstOrDefault(); // sql signature return: Entry
+                _logger.LogDebug("[dbo].[spEntry_ReadEntry] Value: {Value} ActivityId: {ActivityId} returned {Result}", result.Value, result.ActivityId, result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return new Entry();
+        }
+
+        public Entries ReadEntries(string userId, [Optional] DateTime? from, [Optional] DateTime? to)
+        {
+            var result = new Entries();
+            try
+            {
+                result.Data = _db.LoadData<Entry, dynamic>(storedProcedure: "[dbo].[spEntries_Read]", new { userId, from, to }); // sql signature return: collection of Entry
+                _logger.LogDebug("[dbo].[spEntries_Read] Count: {Count} ActivityId: {ActivityId} returned {Result}", result.Data.Count());
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return result;
+        }
+        public bool UpdateEntry(int id, int value)
+        {
+            var result = false;
+            try
+            {
+                result = _db.SaveData(storedProcedure: "[dbo].[spEntry_Update]", new { id, value }); // sql signature return: bit
+                _logger.LogDebug("[dbo].[spEntry_Update] returned {Result}", result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return result;
+        }
+        public bool DeleteEntry(int id)
+        {
+            var result = false;
+            try
+            {
+                result = _db.SaveData(storedProcedure: "[dbo].[spEntry_Delete]", new { id }); // sql signature return: bit
+                _logger.LogDebug("[dbo].[spEntry_Delete] returned {Result}", result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return result;
+        }
+
     }
 }
---
+++ SimpleTracker.DAL/Interfaces/IEntryDal.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTracker.DTO;
+using System.Runtime.InteropServices;
+
+namespace SimpleTracker.DAL.Interfaces
+{
+    public interface IEntryDal
+    {
+        bool CreateNewEntry(Entry entry);
+        bool DeleteEntry(int id);
+        Entries ReadEntries(string userId, [Optional] DateTime? from, [Optional] DateTime? to);
+        Entry ReadEntry(int id, string userId);
+        bool UpdateEntry(int id, int value);
+    }
+}
