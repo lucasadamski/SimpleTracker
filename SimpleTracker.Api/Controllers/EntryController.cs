825f7d9af62f28d546499e3827eab6232ed9594f
Lucas <lucasadams@protonmail.com>
2025-11-20 07:30:51 +0000

    Controller template for Entry

---
+++ SimpleTracker.Api/Controllers/EntryController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : ControllerBase
+    {
+        // GET: api/<EntryController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EntryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EntryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EntryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EntryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
