89ffb6f2846d875639105c0d652105f96d55e580
Lucas <lucasadams@protonmail.com>
2025-11-27 16:53:49 +0000

    New dto

---
+++ SimpleTracker.DTO/EntryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleTracker.DTO
+{
+    public class EntryDto : EntityBase
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; }
+        public string Activity { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
