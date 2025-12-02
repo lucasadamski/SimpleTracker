a99a2d6d7fbaabc79dda7c2e92c130921f03495d
Lucas <lucasadams@protonmail.com>
2025-12-02 08:12:56 +0100

    New table

---
+++ SimpleTracker.DB/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Login] nvarchar(30) not null,
+	[Password] nvarchar(100) not null,
+	[Token] nvarchar(100) null,
+	[RefreshToken] nvarchar(100) null,
+	[RefreshTokenExpiryDate] datetime2 null
+)
