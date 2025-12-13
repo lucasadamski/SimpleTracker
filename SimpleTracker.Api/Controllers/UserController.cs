c65b9ca5c2772b441c2ffdd394c293cc733c5ff4
Lucas <lucasadams@protonmail.com>
2025-12-13 07:28:07 +0100

    New auth logic

--- SimpleTracker.Api/Controllers/UserController.cs
+++ SimpleTracker.Api/Controllers/UserController.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using SimpleTracker.DAL.Interfaces;
 using SimpleTracker.DTO;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace SimpleTracker.Api.Controllers
@@ -21,12 +18,12 @@ namespace SimpleTracker.Api.Controllers
             if (user == null)
                 return BadRequest();
 
-            var existingUser = userDal.ReadUser(user.Login, user.Password); //read user 
+            var existingUser = userDal.ReadUserByToken(user.Token); //read user 
 
-           
-           return NotFound(new { Message = "Incorrect login or password" }); // to do not finished
+            if (existingUser == null)
+               return Login(user);
 
-            
+            return Ok();
         }
 
 
@@ -50,37 +47,6 @@ namespace SimpleTracker.Api.Controllers
             });
         }
 
-
-
-
-        //[HttpPost("authenticate")]
-        //public async IActionResult Authenticate([FromBody] User userObj)
-        //{
-        //    if (userObj == null)
-        //        return BadRequest();
-
-        //    var user = new User();
-
-        //    if (user == null)
-        //        return NotFound(new { Message = "User not found!" });
-
-           
-
-        //    user.Token = CreateJwt(user);
-        //    var newAccessToken = user.Token;
-        //    var newRefreshToken = CreateRefreshToken();
-        //    user.RefreshToken = newRefreshToken;
-        //    user.RefreshTokenExpiryTime = DateTime.Now.AddDays(5);
-        //    await _authContext.SaveChangesAsync();
-
-        //    return Ok(new TokenApiDto()
-        //    {
-        //        AccessToken = newAccessToken,
-        //        RefreshToken = newRefreshToken
-        //    });
-        //}
-
-
         private string CreateJwt(User user)
         {
             var jwtTokenHandler = new JwtSecurityTokenHandler();
@@ -100,35 +66,6 @@ namespace SimpleTracker.Api.Controllers
             };
             var token = jwtTokenHandler.CreateToken(tokenDescriptor);
             return jwtTokenHandler.WriteToken(token);
-        }
-
-        private string CreateRefreshToken()
-        {
-            var tokenBytes = RandomNumberGenerator.GetBytes(64);
-            var refreshToken = Convert.ToBase64String(tokenBytes);
-
-            return refreshToken;
-        }
-
-        private ClaimsPrincipal GetPrincipleFromExpiredToken(string token)
-        {
-            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
-            var tokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateAudience = false,
-                ValidateIssuer = false,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(key),
-                ValidateLifetime = false
-            };
-            var tokenHandler = new JwtSecurityTokenHandler();
-            SecurityToken securityToken;
-            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
-            var jwtSecurityToken = securityToken as JwtSecurityToken;
-            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-                throw new SecurityTokenException("This is Invalid Token");
-            return principal;
-
         }
     }
 }
