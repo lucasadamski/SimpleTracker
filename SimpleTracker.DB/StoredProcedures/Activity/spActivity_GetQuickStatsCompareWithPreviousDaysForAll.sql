aee39c6ed872621d6c543cdbeb4cef7da24b0095
Lucas <lucasadams@protonmail.com>
2026-01-19 08:12:54 +0000

    New sproc

---
+++ SimpleTracker.DB/StoredProcedures/Activity/spActivity_GetQuickStatsCompareWithPreviousDaysForAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spActivity_GetQuickStatsCompareWithPreviousDaysForAll]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
--- SimpleTracker.DB/StoredProcedures/Activity/spActivity_GetQuickStatsForAll.sql
+++ SimpleTracker.DB/StoredProcedures/Activity/spActivity_GetQuickStatsForAll.sql
@@ -5,6 +5,10 @@ AS
 	declare @weekStart date = DATEADD(DAY, -((DATEPART(WEEKDAY, @today) + @@DATEFIRST - 2) % 7), CAST(@today AS date))
 	declare @monthStart date = DATEADD(DAY, 1, EOMONTH(@today, -1))
 
+	declare @yesterday date = dateadd(day, -1, @today)
+	declare @previousWeek date = dateadd(week, -1, @weekStart)
+	declare @previousMonth date = dateadd(month, -1, @monthStart)
+
 	select 
 		 a.Name as [ActivityName]
 		,u.Name as [UnitName]
@@ -16,6 +20,13 @@ AS
 			end
 		) as [TodayValue]
 		,sum(
+			case when cast(e.DateAdded as date) = @yesterday then 
+				e.[Value]
+			else 
+				0
+			end
+		) as [YesterdayValue]
+		,sum(
 			case when cast(e.DateAdded as date) >= @weekStart then 
 				e.[Value]
 			else 
@@ -23,13 +34,27 @@ AS
 			end
 		) as [ThisWeekValue]
 		,sum(
+			case when cast(e.DateAdded as date) >= @previousWeek and cast(e.DateAdded as date) < @weekStart then 
+				e.[Value]
+			else 
+				0
+			end
+		) as [LastWeekValue]
+		,sum(
 			case when cast(e.[DateAdded] as date) >= @monthStart then 
 				e.[Value]
 			else 
 				0
 			end
 		) as [ThisMonthValue]
-		,sum(e.Value) as [AllTimeValue]
+		,sum(
+			case when cast(e.DateAdded as date) >= @previousMonth and cast(e.DateAdded as date) < @monthStart then 
+				e.[Value]
+			else 
+				0
+			end
+		) as [LastMonthValue]
+		,coalesce(sum(e.Value), 0) as [AllTimeValue]
 	from 
 		[dbo].[Activity] a
 			join [dbo].[Unit] u on a.[UnitId] = u.[Id]
